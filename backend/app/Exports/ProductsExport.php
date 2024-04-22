044446c336eb5ce01afe1f2747b599c2793ed5ce
imViniciuuss <imviniciuuss@gmail.com>
2024-04-21 23:17:27 -0300

    adjust

--- backend/app/Exports/ProductsExport.php
+++ backend/app/Exports/ProductsExport.php
@@ -3,16 +3,10 @@
 namespace App\Exports;
 
 use App\Models\Product;
-use Maatwebsite\Excel\Concerns\FromArray;
 use Maatwebsite\Excel\Concerns\FromCollection;
-use Maatwebsite\Excel\Concerns\FromQuery;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
-use Maatwebsite\Excel\Concerns\WithBackgroundColor;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
 use Maatwebsite\Excel\Concerns\WithHeadings;
-use Maatwebsite\Excel\Concerns\WithMapping;
-use Maatwebsite\Excel\Concerns\WithStyles;
-use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 
 class ProductsExport implements FromCollection, WithHeadings, WithHeadingRow, ShouldAutoSize
 {
